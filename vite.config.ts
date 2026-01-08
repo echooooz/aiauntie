1d4e4bf5fa0e255c2ff86d9d96e7a300fc7485de
Echo <37577443+echooooz@users.noreply.github.com>
2026-01-08 15:46:05 +0800

    Fix process.cwd() type in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, (process as any).cwd(), '');
   return {
     plugins: [react()],
     define: {
-      // 这行代码至关重要，它允许你在前端代码中使用 process.env.API_KEY
       'process.env.API_KEY': JSON.stringify(env.API_KEY)
     }
   }
